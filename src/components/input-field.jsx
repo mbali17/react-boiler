55319c020f893317b2806e750e82de7cac9fc530
mbali17 <alimirbasheer@gmail.com>
2020-12-29 20:19:51 -0800

    using shorthand instead of charAt

--- src/components/input-field.jsx
+++ src/components/input-field.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const InputField = ({ captureField, name, triggers }) => <label> {name.replace(name.charAt(0), name.charAt(0).toUpperCase())}: <input type='text' name={name} id={name}
+const InputField = ({ captureField, name, triggers }) => <label> {name.replace(name[0], name[0].toUpperCase())}: <input type='text' name={name} id={name}
 onChange={(e) => captureField(e, triggers)}/>  <br/></label>;
 
 export default InputField;

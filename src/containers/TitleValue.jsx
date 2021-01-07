f25fc60a430f39072c64dc33cfa4119e94e6303d
mbali17 <alimirbasheer@gmail.com>
2021-01-06 21:07:28 -0800

    Updating the title component, removing dispatch actions.

--- src/containers/TitleValue.jsx
+++ src/containers/TitleValue.jsx
@@ -9,19 +9,8 @@ class TitleValue extends Component {
   }
 }
 // Each container needs to have acess to the updated state. Hence, we need to subscribe to the store to listen to the changes.
-const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    number: state.number,
-  };
-};
+const mapStateToProps = (state) => ({
+  number: state.number,
+});
 
-// Each container needs to dispatch events to the store. This is done by mapping to props.
-const mapDispatchToProps = (dispatch) => {
-  console.log(dispatch);
-  return {
-    onIncrement: () => dispatch({ type: 'INCREMENT_NUMBER', value: 1 }),
-    onDecrement: () => dispatch({ type: 'DECREMENT_NUMBER', value: 1 }),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(TitleValue);
+export default connect(mapStateToProps)(TitleValue);
